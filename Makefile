a1799dd1f1d97486ddca79d4121dd56eaf2b9491
Maxime Rauer <maxime.rauer@gmail.com>
2020-04-14 21:07:10 -0500

    [MID] Get list of repos (#2)

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+dev:
+	docker build -t gitdump:latest . && docker run -it --rm -v ${CURDIR}:/usr/src/app gitdump:latest && docker exec -it gitdump:latest
+
 osx:
 	docker run --rm -v ${CURDIR}:/usr/src/app -w /usr/src/app golang:1.13 env GOOS=darwin GOARCH=amd64 go build -i -o gitdump && chmod +x gitdump
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	resp, err := http.Get("https://api.github.com/users/mrauer/repos")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var repos []map[string]interface{}
+	if err := json.Unmarshal(body, &repos); err != nil {
+		panic(err)
+	}
+	fmt.Println(repos[0]["url"])
 }
