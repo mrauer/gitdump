1d787c0caf50435887885943f9f69fb196585e61
Maxime Rauer <maxime.rauer@gmail.com>
2020-05-18 22:17:57 -0500

    [MID] Public Dump

--- lib/api.go
+++ lib/api.go
@@ -20,7 +20,7 @@ type ApiResult struct {
 	Url  string `json:"svn_url"`
 }
 
-func GetRepository(args []string) {
+func GetPublicRepository(args []string) {
 	r, err := client.Get(API_ROOT_USERS + args[0] + "/repos?per_page=1000")
 	if err != nil {
 		panic(err)
@@ -68,7 +68,41 @@ func GetRepository(args []string) {
 		if err = DownloadFile(fmt.Sprintf("%s/%s.zip", path, results[repository].Name), url); err != nil {
 			fmt.Println(err.Error())
 		}
+	}
+}
 
+func GetPublicRepositories(account string) {
+	r, err := client.Get(API_ROOT_USERS + account + "/repos?per_page=1000")
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	data := []ApiResult{}
+
+	err = json.Unmarshal([]byte(b), &data)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Today's directory
+	path := fmt.Sprintf("data/%s/%s", account, time.Now().Format("2006-01-02"))
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, 0700)
+	}
+
+	fmt.Println("\nDownloading all repositories:\n")
+	for i := 0; i < len(data); i++ {
+		fmt.Println(fmt.Sprintf("Downloading %s", data[i].Name))
+		url := fmt.Sprintf("%s/archive/master.zip", data[i].Url)
+		if err = DownloadFile(fmt.Sprintf("%s/%s.zip", path, data[i].Name), url); err != nil {
+			fmt.Println(err.Error())
+		}
 	}
 }
 
